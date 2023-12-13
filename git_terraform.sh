f43866d313ca7724e80e2fb36f7ccfedae34ba90
Stan Grigorov <user.name@gmail.com>
2023-12-12 23:18:03 -0700

    test sh script

--- git_terraform.sh
+++ git_terraform.sh
@@ -1,4 +1,11 @@
-git pull git@gitlab.com:sgrigorov/terraform.git
-sed -i '/	url = git@gitlab.com:sgrigorov\/terraform.git/a \	url = git@github.com:sgrigorov\/terraform.git' ~/scripts/terraform/.git/config
+REPOSITORY="terraform"
+GITHUB="url = git@github.com:sgrigorov/$REPOSITORY.git"
+GITLAB="url = git@gitlab.com:sgrigorov/$REPOSITORY.git"
+
+FILE='.git/config'
+
+grep -qF -- "$GITLAB" "$FILE" || sed -i "/remote \"origin\"/a \	$GITLAB" "$FILE"
+grep -qF -- "$GITHUB" "$FILE" || sed -i "/remote \"origin\"/a \	$GITHUB" "$FILE"
+
 git config commit.template ~/scripts/terraform/.gitmessage
 echo " My commit  message" > ~/scripts/terraform/.gitmessage
